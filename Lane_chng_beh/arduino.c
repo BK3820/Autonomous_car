4e3f89e72c3f9194671ce69a5322805b86ce78b9
BALAKUMAR P <80500214+BK3820@users.noreply.github.com>
2021-07-19 07:06:27 +0530

    Create arduino.c

---
+++ Lane_chng_beh/arduino.c
@@ -0,0 +1,19 @@
+int i =0;
+int j =0;
+   
+const int EnableL = 5;
+const int HighL = 6;       // LEFT SIDE MOTOR
+const int LowL =7;
+
+const int EnableR = 10;
+const int HighR = 8;       //RIGHT SIDE MOTOR
+const int LowR =9;
+
+const int D0 = 0;       //Raspberry pin 21    LSB
+const int D1 = 1;       //Raspberry pin 22
+const int D2 = 2;       //Raspberry pin 23
+const int D3 = 3;       //Raspberry pin 24    MSB
+
+int a,b,c,d,data;
+
+
