e2c000df651e989cec46d48a908ebacd4ffacb3f
BALAKUMAR P <80500214+BK3820@users.noreply.github.com>
2021-09-21 12:21:34 +0530

    Update arduino.c

--- Lane_chng_beh/arduino.c
+++ Lane_chng_beh/arduino.c
@@ -2,19 +2,19 @@ int i =0;
 int j =0;
    
 const int EnableL = 5;
-const int HighL = 6;       // LEFT SIDE MOTOR
+const int HighL = 6;       
 const int LowL =7;
 
 
 const int EnableR = 10;
-const int HighR = 8;       //RIGHT SIDE MOTOR
+const int HighR = 8;       
 const int LowR =9;
 
 
-const int D0 = 0;       //Raspberry pin 21    LSB
-const int D1 = 1;       //Raspberry pin 22
-const int D2 = 2;       //Raspberry pin 23
-const int D3 = 3;       //Raspberry pin 24    MSB
+const int D0 = 0;       
+const int D1 = 1;       
+const int D2 = 2;       
+const int D3 = 3;       
 
 int a,b,c,d,data;
 
